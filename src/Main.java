c7aaf543c440e4901147332c0924cdb82ffc39d5
Nathan Goodlett <nathangoodlett@dhcp-172-30-238-248.guest.uc.edu>
2022-09-22 14:56:38 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Please enter your age");
+        int age = 19;
+
+        if(age >= 21)
+        {
+            System.out.println("You receive a wristband!");
+        }
+        else;
+
+    }
+}
